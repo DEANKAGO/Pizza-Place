8dccef9198cb5d403d07120b5eb6c7d26e6dd760
Dean Kago <martokhago@gmail.com>
2022-03-21 20:04:05 +0300

    added multiple selection for toppinggs

--- scripts.js
+++ scripts.js
@@ -23,20 +23,31 @@ $(document).ready(function() {
   $(".btn-no").hide();
   $(".feedback h5 ").hide();
 
+  function computeTotal(selected) {
+    let tot = selected.map((value) => Number(value)).reduce((prev, curr) => prev + curr, 0);
+     return tot;
+  }
+
 
   $(".btn1").click(function(){
+
     var pizzaSize = $("#size option:selected").val();
-    var pizzaToppings = $("#toppings option:selected").val();
+    var pizzaToppings = $("#toppings").val();
+    console.log(pizzaToppings)
     var pizzaCrust = $("#crust option:selected").val();
-    var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+    var total = parseInt(pizzaSize) + parseInt(computeTotal(pizzaToppings)) + parseInt(pizzaCrust);
+
 
     var order = 1;
     var grandTotal =0;
+
     
     $("#size1").html($("#size option:selected").text() + "-" +pizzaSize);
-    $("#toppings1").html($("#toppings option:selected").text() + "-" +pizzaToppings);
+    $("#toppings1").html($("#toppings option:selected").text() + "-" +computeTotal(pizzaToppings));
     $("#crust1").html($("#crust option:selected").text() + "-" +pizzaCrust);
     $("#total").html(total);
+
+
 
     function Pizza(size, toppings, crust, total, orderNo) {
       this.size = size;
@@ -44,7 +55,9 @@ $(document).ready(function() {
       this.crust = crust;
       this.total = total;
       this.orderNo = orderNo;
+
     }
+   
 
     $("table").show()
     $(".buttons").show()
@@ -54,15 +67,15 @@ $(document).ready(function() {
 
     $(".btn2").click(function(){
       var pizzaSize = $("#size option:selected").val();
-      var pizzaToppings = $("#toppings option:selected").val();
+      var pizzaToppings = $("#toppings").val();
       var pizzaCrust = $("#crust option:selected").val();
-      var total = parseInt(pizzaSize) + parseInt(pizzaToppings) + parseInt(pizzaCrust);
+      var total = parseInt(pizzaSize) + parseInt(computeTotal(pizzaToppings)) + parseInt(pizzaCrust);
       order = order + 1;
       grandTotal = grandTotal + total;
 
       var newPizza = new Pizza(pizzaSize, pizzaToppings, pizzaCrust, total, order);
 
-      var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size1">' + $("#size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings1">' + $("#toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust1">' + $("#crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+      var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size1">' + $("#size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings1">' + $("#toppings option:selected").text() + " - " + computeTotal(newPizza.toppings) + '</td><td id="crust1">' + $("#crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
 
       $("#table1").append(newRow);
 
@@ -84,8 +97,8 @@ $(document).ready(function() {
 
 
     $(".btn-yes").click(function() {
-      $(".feedback h4").hide();
       $(".btn-yes").hide();
+      $(".feedback h4").hide();
       $(".btn-no").hide();
       $(".feedback h2").hide()
       $(".feedback .pick-up").show();
@@ -94,11 +107,10 @@ $(document).ready(function() {
 
 
      $(".btn-no").click(function() {
-      $(".feedback h4").hide()
       $(".btn-yes").hide()
+      $(".feedback h4").hide()
       $(".btn-no").hide()
       $(".feedback h2").show()
-      // $(".feedback .pick-up").show()
 
     })
 
